b237870c63e061d1849763489af64d10fda9057c
nishant-cyro <nishant.tuteja@vinsol.com>
2016-07-11 13:30:05 +0530

    Modify quantifier_decorator.rb

--- app/models/spree/stock/quantifier_decorator.rb
+++ app/models/spree/stock/quantifier_decorator.rb
@@ -2,10 +2,8 @@ module Spree
   module Stock
     Quantifier.class_eval do
 
-      alias_method :original_can_supply?, :can_supply?
-
       def can_supply?(required=1)
-        original_can_supply?(required) || Spree::Variant.find(@variant).product.is_gift_card?
+        super || Spree::Variant.find(@variant).product.is_gift_card?
       end
     end
   end
