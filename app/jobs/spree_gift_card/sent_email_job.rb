0da94f03d1ceb1d1e7e94d0d10ff669162448336
Wojtek <swierczynskiwojtek@gmail.com>
2017-09-25 12:29:09 +0200

    fix sending email

--- app/jobs/spree_gift_card/sent_email_job.rb
+++ app/jobs/spree_gift_card/sent_email_job.rb
@@ -3,7 +3,7 @@ module SpreeGiftCard
     queue_as :default
 
     def perform(*_args)
-      gift_cards = Spree::GiftCard.active.has_springboard_id.where.not(delivery_on: nil)
+      gift_cards = Spree::GiftCard.active.has_springboard_id.where.not(delivery_on: nil).where("delivery_on < ? and sent_at is null", Time.now)
       gift_cards.each do |gift_card|
         if gift_card.line_item.is_e_gift_card?
           Spree::OrderMailer.gift_card_email(gift_card.id, gift_card.line_item.order).deliver
