6fdadc176c451f19dcc42b55fb6f6d71a8b99331
pstrzalk <pawel@itideas.pl>
2018-01-04 11:44:49 +0100

    Allow GC integration check before capture

--- app/controllers/spree/user_gift_cards_controller.rb
+++ app/controllers/spree/user_gift_cards_controller.rb
@@ -4,12 +4,15 @@ module Spree
 
     def create
       code = params[:code]
-      redirect_to action: :new if code.blank?
+      if code.blank?
+        flash[:error] = 'Invalid Gift Card code.'
+        redirect_to action: :new
+      end
 
       # Check if GC is defined in Spree
-      spree_gift_card = Spree::GiftCard.find_by_code(code)
+      gift_card = Spree::GiftCard.find_by_code(code)
       if spree_gift_card.present?
-        spree_current_user.gift_cards << spree_gift_card
+        spree_current_user.gift_cards << gift_card
         flash[:success] = 'Gift Card added'
         # If GC is not defined in Spree, check if it is defined in integrated services
       elsif !import_integrated_gift_card
@@ -21,8 +24,6 @@ module Spree
     end
 
     def index
-      # Update balance for all the Gift Cards
-      spree_current_user.springboard_import_gift_cards_balance
       @gift_cards = spree_current_user.gift_cards
     end
 
